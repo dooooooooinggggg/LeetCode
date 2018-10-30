42237b20f213d55e493101df34becbd0b77e2053
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-10-30 18:35:05 +0900

    Sloppy Commit

---
+++ 008_StringToInteger.py
@@ -0,0 +1,12 @@
+import re
+
+
+class Solution:
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        s = str
+        for i in range(len(s)):
+            if(s[i] == " " or)

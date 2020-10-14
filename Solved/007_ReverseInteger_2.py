21c46570010467b3b557fc01d2290288c10f7a3f
Tatsunori Ishikawa <dooooooooinggggg@gmail.com>
2020-10-14 16:33:26 +0900

    10/14

---
+++ Solved/007_ReverseInteger_2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        last_x  =1
+        if x < 0:
+            last_x=-1
+        ab_value = x * last_x
+        ab_value_str = ''.join(list(reversed(str(ab_value))))
+        r = int(ab_value_str)*last_x
+        minimum = 2**31 * -1
+        maximum = 2**31 - 1
+        if r < minimum or r > maximum:
+            return 0
+
+        return r
---
+++ Solved/387_FirstUniqueCharacterinaString.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isUnique(self,finder:str, findee:str) -> bool:
+        for f in findee:
+            if f == finder:
+                return False
+        return True
+
+    def firstUniqChar(self, s: str) -> int:
+
+        if len(s) == 1:
+            return 0
+
+        for c_index in range(len(s)):
+            compared_s_before = s[0:c_index]
+            compared_s_after = s[c_index+1:len(s)]
+            if self.isUnique(s[c_index], compared_s_before+compared_s_after):
+                return c_index
+        return -1

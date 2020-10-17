af0d656e44d6824596e5bd63a38479257235637a
Tatsunori Ishikawa <dooooooooinggggg@gmail.com>
2020-10-17 16:13:19 +0900

    10/17

---
+++ Solved/014_LongestCommonPrefix.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/887/
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+
+        if len(strs) == 1:
+            return strs[0]
+        
+        str_0 = strs[0]
+        striped_strs = strs[1:len(strs)]
+
+        past_prefix = ""
+        for i in range(1,len(str_0)+1):
+            for s in striped_strs:
+                if s[0:i] != str_0[0:i]:
+                    return past_prefix
+            past_prefix = str_0[0:i]
+
+        return past_prefix
